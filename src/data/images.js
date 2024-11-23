e76bde0a6d305ca6dbc5d1612332e4ee13055bac
José Carlos Fernández Rodríguez <jcfernandez@Joses-MacBook-Pro-2.local>
2024-11-23 13:26:09 +0100

    ImageGrid implemented and some general styling

---
+++ src/data/images.js
@@ -0,0 +1,24 @@
+// src/data/images.js
+import image1 from '../images/IMG_1532.jpg';
+import image2 from '../images/EDA7CA04-D8D7-42EC-B28A-FDEA5B01CD75-1220-000000CBA1A07FA2.jpg';
+import image3 from '../images/IMG_1150.jpg';
+import image4 from '../images/IMG_1665.jpg';
+
+export const imageList = [
+  {
+    src: image1,
+    alt: "Description 1"
+  },
+  {
+    src: image2,
+    alt: "Description 2"
+  },
+  {
+    src: image3,
+    alt: "Description 3"
+  },
+  {
+    src: image4,
+    alt: "Description 3"
+  },
+];
